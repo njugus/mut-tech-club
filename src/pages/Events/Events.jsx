3b7a68debc49ac2d049842f2af28d5d5ae33f55b
njugus <kayvohnjuguna@gmail.com>
2024-06-02 20:20:45 -0700

    added the tracks section

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,7 +1,22 @@
+import './Events.css'
+
 function Events() {
     return (
         <>
-        <p>Events Page</p>
+        <section className="Events">
+            <div className="Events-Wrapper">
+                <div className="event-card">
+                    <h1 className="event-heading">Monday</h1>
+                    <p>Event: CyberSecurity</p>
+                    <p>Time: 7pm to 9pm</p>
+                    <p>Venue:  lab F04</p>
+                    <p>Lead: Webster Ifedha</p>
+                </div>
+            </div>
+
+        </section>
+
+
         </>
     )
 }
